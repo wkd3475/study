6b6dc17198aa5b18ec5df5b79337672dd85aea78
wkd3475 <wkd3475@gmail.com>
2019-01-10 16:15:50 +0900

    complete

--- c_c++_projects/baekjoon/b_10250/b_10250.cpp
+++ c_c++_projects/baekjoon/b_10250/b_10250.cpp
@@ -11,7 +11,8 @@ int main() {
 
     for (int i = 0; i < T; i++) {
         scanf("%d %d %d", &H, &W, &N);
-
-            
+        num_floor = N % H != 0 ? N % H : H ;
+        num_room = N / H != 0 ? N / H + 1 : N / H;
+        printf("%d\n", num_floor * 100 + num_room);
     }
 }
