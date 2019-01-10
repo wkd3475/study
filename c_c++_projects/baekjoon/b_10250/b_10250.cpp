ab89fd57b3549e4207f57fd0be54a8833ee10ac6
wkd3475 <wkd3475@gmail.com>
2019-01-10 16:02:40 +0900

    solving start

---
+++ c_c++_projects/baekjoon/b_10250/b_10250.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// number of roon => W
+// floor => H
+// customer num => N
+
+int main() {
+    int T, H, W, N;
+    int num_floor, num_room;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        scanf("%d %d %d", &H, &W, &N);
+
+            
+    }
+}
