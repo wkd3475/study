f5362a7998d610f7eb6258e5fef0997fcbc5d2d0
wkd3475 <wkd3475@gmail.com>
2019-01-18 03:09:21 +0900

    test

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+
+	host:'localhost'
+
+	,user:'wkd3475'
+
+    ,password:'2rnlska',
+
+    port: 3306,
+    
+    database: ''
+
+});
+
+connection.connect();
+
+connection.query('use test;');
+
 function change() {
      $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  }
 
- function login() {
+function login() {
      
  }
 
- function accountCreate() {
-
+function accountCreate() {
+    connection.query("insert into user (user_name, user_password, user_email_address) values('test', 'test123', 'test@gmail.com');")
  }
